888108948a793676e6a90d102b2815a40020d1a9
Piyumi Weeraarachchi <53691448+PiyumiUoR@users.noreply.github.com>
2023-11-23 12:25:28 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,66 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "thread.h"
+#include "ztimer.h"
+#include "shell.h"
+
+#include "mutex.h"
+
+#include "lpsxxx.h"
+#include "lpsxxx_params.h"
+
+
+static lpsxxx_t lpsxxx;
+
+
+static void _lpsxxx_usage(char *cmd)
+{
+    printf("usage: %s <temperature|pressure>\n", cmd);
+}
+
+static int lpsxxx_handler(int argc, char *argv[])
+{
+    if (argc < 2) {
+        _lpsxxx_usage(argv[0]);
+        return -1;
+    }
+
+     if (!strcmp(argv[1], "temperature")) {
+        int16_t temp = 0;
+        lpsxxx_read_temp(&lpsxxx, &temp);
+        printf("Temperature: %i.%u°C\n", (temp / 100), (temp % 100));
+    }
+    else if (!strcmp(argv[1], "pressure")) {
+        uint16_t pres = 0;
+        lpsxxx_read_pres(&lpsxxx, &pres);
+        printf("Pressure: %uhPa\n", pres);
+    }
+    else {
+        _lpsxxx_usage(argv[0]);
+        return -1;
+    }
+
+    return 0;
+}
+
+static const shell_command_t commands[] = {
+    
+    { "lps", "read the lpsxxx values", lpsxxx_handler },
+
+
+    { NULL, NULL, NULL}
+};
+
+int main(void)
+{
+    
+    lpsxxx_init(&lpsxxx, &lpsxxx_params[0]);
+
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    return 0;
+}
 
